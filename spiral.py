ca743f6ecb26b7184c5f60073f6c2bbca06d9077
Konstantinos <kyassemis@gmail.com>
2020-01-21 17:46:55 +0200

    Add files via upload

---
+++ spiral.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+def myfunc(length=5):
+    speed(0)
+    shape('turtle')
+    for i in range(0, 60):
+        for x in range(0, 4):
+            forward(length)
+            right(90)
+        right(5)
+        length += 5
+    
+myfunc()
